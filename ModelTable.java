7992db58ee31c0ca35f7e3f6de432b3d521104d2
jsm6437 <jsm6437@naver.com>
2022-01-25 21:09:38 +0900

    first commit

---
+++ ModelTable.java
@@ -0,0 +1,17 @@
+package application;
+
+public class ModelTable {
+	String food;
+	
+	public ModelTable(String food) {
+		this.food = food;
+	}
+
+	public String getFood() {
+		return food;
+	}
+
+	public void setFood(String food) {
+		this.food = food;
+	}
+}
